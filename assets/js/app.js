9102eb4c623cc82a3bf713a5ea0f461c69ac2242
mvaldetaro <mvaldetaro@gmail.com>
2014-06-02 17:43:42 -0300

    JavaScript Constants defined

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+var defaults = {
+  // CSS selectors and attributes that would be used by the JavaScript functions
+  todoTask: "todo-task",
+  todoTitle: "task-title",
+  todoDate: "task-date",
+  todoDescription: "task-description",
+  taskId: "task-",
+  formId: "todo-form",
+  dataAttribute: "data",
+  deleteDiv: "delete-area"
+}, codes = {
+  "1" : "#pending", // For pending tasks
+  "2" : "#progress",
+  "3" : "#completed"
+};
