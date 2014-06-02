aa4dc509cbd11dacc1c33d7feb5f3248c28db04e
mvaldetaro <mvaldetaro@gmail.com>
2014-06-02 18:27:14 -0300

    Function "generateTask" added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var defaults = {
   // CSS selectors and attributes that would be used by the JavaScript functions
-  todoTask: "todo-task",
+  todoTask: "task-item",
   todoTitle: "task-title",
   todoDate: "task-date",
   todoDescription: "task-description",
@@ -13,3 +13,33 @@ var defaults = {
   "2" : "#progress",
   "3" : "#completed"
 };
+
+// Add Task
+
+var generateTask = function(params){
+  var parent = $(codes[params.code]),
+      wrapper;
+
+  if(!parent) {
+    return;
+  }
+
+  wrapper = $("<li />", {
+    "class" : defaults.todoTask,
+    "id" : defaults.taskId + params.id,
+    "data" : params.id
+  }).appendTo(parent);
+
+  $("<div />", {
+    "class" : defaults.todoTitle,
+    "text": params.title
+  }).appendTo(wrapper);
+}
+
+generateTask({
+  id: "123",
+  code: "1",
+  title: "Uma tarefa para teste"
+});
+
+console.log("Chegando aqui!");
