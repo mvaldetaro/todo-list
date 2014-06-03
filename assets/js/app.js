27071b817fc87366af060132442d383f923baa87
mvaldetaro <mvaldetaro@gmail.com>
2014-06-03 11:28:23 -0300

    Delete Task added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ var defaults = {
   taskId: "task-",
   formId: "todo-form",
   dataAttribute: "data",
-  deleteDiv: "delete-area"
+  deleteArea: "delete-area"
 }, codes = {
   "1" : "#pending", // For pending tasks
   "2" : "#progress",
@@ -17,6 +17,67 @@ var defaults = {
 var data = JSON.parse(localStorage.getItem("todoData"));
 
 data = data || {};
+
+// Init App
+var init = function (options) {
+
+  //Load Local Storage
+  options = options || {};
+  options = $.extend({}, defaults, options);
+
+  $.each(data, function (index, params) {
+      generateTask(params);
+  });
+
+  // Adding drop function to each category of task
+  $.each(codes, function(index, value) {
+    $(value).droppable({
+      drop: function(event, ui) {
+        var element = ui.helper,
+            css_id = element.attr("id"),
+            id = css_id.replace(options.taskId, ""),
+            object = data[id];
+
+        // Removing old element
+        removeTask(object);
+
+        // Changing object code
+        object.code = index;
+
+        // Generating new element
+        generateTask(object);
+
+        // Updating Local Storage
+        data[id] = object;
+        localStorage.setItem("todoData", JSON.stringify(data));
+
+        // Hiding Delete Area
+        $("#" + defaults.deleteArea).hide();
+      }
+    });
+  });
+
+  // Adding drop function to delete task
+  $("#" + options.deleteArea).droppable({
+    drop: function(event, ui) {
+      var element = ui.helper,
+          css_id = element.attr("id"),
+          id = css_id.replace(options.taskId, ""),
+          object = data[id];
+
+      // Removing old element
+      removeTask(object);
+
+      // Updating local storage
+      delete data[id];
+      localStorage.setItem("todoData", JSON.stringify(data));
+
+      // Hiding Delete Area
+      $("#" + defaults.deleteArea).hide();
+    }
+  });
+}
+
 
 // Add Task
 
@@ -41,17 +102,24 @@ var generateTask = function(params){
 
   wrapper.draggable({
     start: function() {
-      $("." + defaults.deleteDiv).show();
+      $("#" + defaults.deleteArea).fadeIn(160);
     },
     stop: function() {
-      $("." + defaults.deleteDiv).hide();
+      $("#" + defaults.deleteArea).fadeOut(160);
+    },
+    revert: "invalid",
+    revertDuration : 160,
+    cursor: "move",
+    cursorAt: {
+      top: 17,
+      left: 183
     }
   });
 
 }
 
 //Remove Task
-var removeElement = function(params) {
+var removeTask = function(params) {
   $("#" + defaults.taskId + params.id).remove();
 };
 
@@ -80,17 +148,15 @@ var addItem = function() {
     title: title
   };
 
-  // Generate Todo Element
-  generateTask(tempData);
-
   // Saving element in local storage
   data[id] = tempData;
   localStorage.setItem("todoData", JSON.stringify(data));
 
+  // Generate Todo Element
+  generateTask(tempData);
+
   // Reset Form
   inputs[0].value = "";
-
-  console.log("Opa!");
 
 }
 
@@ -101,57 +167,6 @@ function enter(e){
     addItem();
     event.preventDefault();
   }
-}
-
-// Init App
-var init = function (options) {
-
-  $("." + defaults.deleteDiv).hide();
-
-  //Load Local Storage
-  options = options || {};
-  options = $.extend({}, defaults, options);
-
-  $.each(data, function (index, params) {
-      generateTask(params);
-  });
-
-  //Drag and Drop
-  $("." + defaults.todoTask).draggable({
-    cursor: "move",
-    cursorAt: {
-      top: 17,
-      left: 183
-    }
-  });
-
-  // Adding drop function to each category of task
-  $.each(codes, function(index, value) {
-    $(value).droppable({
-      drop: function(event, ui) {
-        var element = ui.helper,
-            css_id = element.attr("id"),
-            id = css_id.replace(options.taskId, ""),
-            object = data[id];
-
-        // Removing old element
-        removeElement(object);
-
-        // Changing object code
-        object.code = index;
-
-        // Generating new element
-        generateTask(object);
-
-        // Updating Local Storage
-        data[id] = object;
-        localStorage.setItem("todoData", JSON.stringify(data));
-
-        // Hiding Delete Area
-        $("#" + defaults.deleteDiv).hide();
-      }
-    });
-  });
 }
 
 init();
